a3557b681cc48dfbace6fd672503aa02f4b527f1
kevel choumene <k.choumene@cloudfacile.com>
2025-01-09 12:21:18 +0100

    remove debug

--- policy/services.py
+++ policy/services.py
@@ -123,27 +123,19 @@ class PolicyService:
         receipt = data.pop("receipt", None)
         payer_uuid = data.pop("payer_uuid", None)
         data = self._clean_mutation_info(data)
-        print("Val ", PolicyConfig.one_policy_per_program)
         if PolicyConfig.one_policy_per_program == True:
-            print("OK......")
             if "product_id" in data:
                 product = Product.objects.get(id=data["product_id"])
-                print("Produit 1 ", product)
                 program = product.program
-                print("program: ", program)
                 if program:
                     for police in Policy.objects.filter(family=data["family_id"]).filter(validity_to__isnull=True):
-                        print("police actuelle ", police.status)
                         if police.status == Policy.STATUS_ACTIVE:
                             prod = Product.objects.get(id=police.product.id)
-                            print("prod ", prod)
                             if prod:
-                                print("comparaison ", program.idProgram, " et ", prod.program.idProgram)
                                 if program.idProgram == prod.program.idProgram:
                                     raise Exception("Vous ne pouvez pas avoir plusieurs polices actives pour un même programme")
                                 # If the policy that the user had previously is a cheque sante,
                                 # he cannot have a fagep policy
-                                print("Comparaison ", prod.program.code, " et ", program.code)
                                 if prod.program.code == "CCS" and program.code == "PAL":
                                     raise Exception("L'assuré ne peux pas avoir un proegramme FAGEP"\
                                     " s'il a déja un proegramme Cheque santé actif")
