49e84b270d15fbdd3f8b2673d8ff1ba39fa94236
kevel choumene <k.choumene@cloudfacile.com>
2026-07-14 15:13:36 +0100

    feat(ForcedExpiration): add perms and forced expiration mutation ref37437

--- policy/services.py
+++ policy/services.py
@@ -207,7 +207,6 @@ class PolicyService:
         try:
             policy.save_history()
             policy.status = Policy.STATUS_SUSPENDED
-            policy.expiry_date = py_datetime.now().date()
             policy.audit_user_id = user.id_for_audit
             policy.save()
             return []
@@ -223,6 +222,7 @@ class PolicyService:
         try:
             policy.save_history()
             policy.status = Policy.STATUS_EXPIRED
+            policy.expiry_date = py_datetime.now().date()
             policy.stage = Policy.STAGE_EXPIRATION_FORCED
             policy.audit_user_id = user.id_for_audit
             policy.save()
