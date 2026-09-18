fb62851b1025ca3153d49055bbc8903e877133b6
kevel choumene <k.choumene@cloudfacile.com>
2026-09-18 18:01:57 +0100

    feat: remove print

--- policy/tests/test_policy_create_invoice.py
+++ policy/tests/test_policy_create_invoice.py
@@ -19,8 +19,6 @@ class TestPolicyInvoice(TestCase):
         self.product = create_test_product(code="TST-HPD1")
         self.user = create_test_interactive_user()
         self.service = PolicyService(self.user)
-        print("created user ", self.user)
-        print("created user ID", self.user.id)
     @patch("policy.services.update_insuree_policies")
     @patch("invoice.services.invoice.InvoiceService.create")
     def test_create_policy_should_activate_free_policy(
@@ -123,7 +121,6 @@ class TestPolicyInvoice(TestCase):
             [(None, Decimal("1000"))],
             [(None, Decimal("0"))],
         ]
-        print("Policy: ", policy)
         with patch(
             "policy.services.CALCULATION_RULES",
             [calc_rule]
